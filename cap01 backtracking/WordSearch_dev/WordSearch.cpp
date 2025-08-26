cbe8c20878989c35a9f6d5f01dbcda7ec51f8956
Andrés Melgar <amelgar@pucp.edu.pe>
2025-08-26 14:12:58 -0500

    clase

--- cap01 backtracking/WordSearch_dev/WordSearch.cpp
+++ cap01 backtracking/WordSearch_dev/WordSearch.cpp
@@ -21,12 +21,36 @@ void WordSearch::mostrar_tablero() const {
 }
 
 bool WordSearch::buscar_palabra(const string &palabra, int fil, int col, int indice) {
-    //TODO
+    if (indice == palabra.length())
+        return true;
+
+    //verifico que no se salga del tablero o que no coincida la letra
+    if (fil < 0 || col < 0 || fil >= this->tablero.size() || col >= this->tablero[0].size()
+        || this->tablero[fil][col] != palabra[indice])
+        return false;
+
+    char caracter_original = this->tablero[fil][col];
+    this->tablero[fil][col] = VISITADO;
+
+    for (const auto &[dx, dy]: this->movimientos) {
+        int nueva_fila = fil + dx;
+        int nueva_col = col + dy;
+        if (buscar_palabra(palabra, nueva_fila, nueva_col, indice+1))
+            return true;
+    }
+    this->tablero[fil][col] = caracter_original; //bactracking
     return false;
 }
 
 bool WordSearch::encontrar_solucion(string palabra) {
-    //TODO
+    int filas = this->tablero.size();
+    int columnas = this->tablero[0].size();
+    for (int i = 0; i < filas; ++i) {
+        for (int j = 0; j < columnas; ++j) {
+            if (buscar_palabra(palabra, i, j, 0))
+                return true;
+        }
+    }
     return false;
 }
 
